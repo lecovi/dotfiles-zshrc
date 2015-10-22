77b159fb886be2824a441ef4d6c742f1121c614b
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2015-10-22 08:57:59 -0300

    Adding TODO to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ZSHRC=~/.zshrc
 #echo $0 returns shell binary path.
-
+# TODO: check if zsh is the SHELL
+# TODO: check if Oh My Zsh is installed
+# TODO: check if zsh-syntax-highlighted is installed
+#       git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+#       ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 if [ ! -f ZSHRC ]; then
     mv ~/.zshrc{,.old}
     ln -s $PWD/.zshrc ~/.zshrc
